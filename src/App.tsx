a306bdb663a56295f0eec129b68a0f3711dbb97e
Azaken1248 <suvkrm@gmail.com>
2026-04-04 03:15:51 +0530

    Integrate to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/analytics/Dashboard";
 import Home from "./pages/home/Home";
+import Profile from "./pages/members/Profile";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analytics" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:idWithExtension" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
